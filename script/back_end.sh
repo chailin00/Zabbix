fff85f3b72a7226f80c1a336ca9f2dc88eb029a7
chailin <165104029@qq.com>
2016-03-09 17:17:51 +0800

    new

--- script/back_end.sh
+++ script/back_end.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dbhost='133.128.174.68'
 dbuser='sxgkzs'
-dbpasswd='sxgksjkdl123!@#'
+dbpasswd='pwd'
 dbport='9066'
 sql='Show @@backend'
 /mysql/component/mysql01/bin/mysql -u$dbuser -p$dbpasswd -P$dbport -h$dbhost -Ne "$sql" 2>/dev/null | grep -c 'Processor'  
--- script/cps_wo_cache_max.sh
+++ script/cps_wo_cache_max.sh
@@ -2,7 +2,7 @@
 #
 dbhost='133.128.174.68'
 dbuser='sxgkzs'
-dbpasswd='sxgksjkdl123!@#'
+dbpasswd='pwd'
 dbport='9066'
 sql='Show @@cache'
 /mysql/component/mysql01/bin/mysql -u$dbuser -p$dbpasswd -P$dbport -h$dbhost -Ne "$sql" 2>/dev/null | grep -n 'CPS_WO' |  awk '{print $2}'
